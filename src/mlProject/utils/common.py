08d1d6303f3bd2f9dbbdca147ed47279a39c01a3
Jody-Ann <drjodyannjones@gmail.com>
2023-09-23 13:35:15 -0500

    Update repo

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,24 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from mlProject import logger
+import json
+import joblib
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Read yaml file and return a ConfigBox object."""
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
